3ec9f6643136a9264908b5edaabd7bda534bb4f4
Kyanlulw <datlc81@gmail.com>
2025-12-07 18:53:41 +0700

    vl

--- transformer.py
+++ transformer.py
@@ -23,8 +23,8 @@ class Transformer(nn.Module):
         src_seq = self.src_embedding(src_seq) # (B, seq_len, d_model)
         trg_seq = self.trg_embedding(trg_seq) # (B, seq_len, d_model)
 
-        src_seq = self.postitional_encoding(src_seq) # (B, seq_len, d_model)
-        trg_seq = self.postitional_encoding(trg_seq) # (B, seq_len, d_model)
+        src_seq = self.positional_encoding(src_seq) # (B, seq_len, d_model)
+        trg_seq = self.positional_encoding(trg_seq) # (B, seq_len, d_model)
 
         src_enc_output = self.encoder(src_seq, enc_mask) # (B, seq_len, d_model)
         trg_dec_output = self.decoder(trg_seq, src_enc_output, enc_mask, dec_mask) # (B, seq_len, d_model)
