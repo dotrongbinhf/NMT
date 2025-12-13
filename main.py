15edec1f394c68fb623b5db7029cefeb68b7d8b8
Kyanlulw <datlc81@gmail.com>
2025-12-13 13:11:29 +0700

    cdmcvcádlaks

--- main.py
+++ main.py
@@ -605,7 +605,7 @@ if __name__=='__main__':
     parser.add_argument('--input', type=str, required=False, help="input sentence when inferencing")
     parser.add_argument('--decode', type=str, required=True, default="greedy", help="greedy or beam?")
     parser.add_argument('--dataset_name', type=str, required=False, help="path to config file")
-    parser.add_argument('--num_epochs', type=str, required=False, help="path to config file")
+    parser.add_argument('--num_epochs', type=int, required=False, help="path to config file")
 
     # parser.add_argument('--use_rope', type=str2bool, default = USE_ROPE, required=True, help="use rope or pe")
     args = parser.parse_args()
